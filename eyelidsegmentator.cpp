bbe662299d1e7c2d5ff2d2f9d1c2949eef3a3b09
Marcelo <marcelo@desktop.(none)>
2009-06-13 23:32:53 -0300

    performance changes

--- eyelidsegmentator.cpp
+++ eyelidsegmentator.cpp
@@ -22,92 +22,71 @@ std::pair<Parabola, Parabola> EyelidSegmentator::segmentEyelids(const Image* ima
 	assert(image->origin == 0);
 
 	CvMat matWorkingRegion;
-	int r = irisCircle.radius * 2.5;
+	int r = irisCircle.radius * 1.5;
 	int x0 = std::max(0, irisCircle.xc-r);
 	int x1 = std::min(image->width, irisCircle.xc+r);
-	int y0 = std::max(0, irisCircle.yc-r);
-	int y1 = std::min(image->height, irisCircle.yc+r);
+	int y0upper = std::max(0, irisCircle.yc-r);
+	int y1upper = irisCircle.yc;
+	int y0lower= irisCircle.yc;
+	int y1lower = std::min(image->height, irisCircle.yc+r);
 
-	cvGetSubRect(image, &matWorkingRegion, cvRect(x0, y0, x1-x0, y1-y0));
-	IplImage workingRegion;
-	cvGetImage(&matWorkingRegion, &workingRegion);
-
-	Parabola upperEyelid = this->segmentUpper(&workingRegion, pupilCircle.radius);
-	Parabola lowerEyelid = this->segmentLower(&workingRegion, pupilCircle.radius);
+	Parabola upperEyelid = this->segmentUpper(image, x0, y0upper, x1, y1upper, pupilCircle, irisCircle);
+	Parabola lowerEyelid = this->segmentLower(image, x0, y0lower, x1, y1lower, pupilCircle, irisCircle);
 
 	std::pair<Parabola, Parabola> result;
-	result.first = Parabola(upperEyelid.x0+x0, upperEyelid.y0+y0, upperEyelid.p);
-	result.second = Parabola(lowerEyelid.x0+x0, lowerEyelid.y0+y0, lowerEyelid.p);
+	result.first = upperEyelid;
+	result.second = lowerEyelid;
 	return result;
 }
 
-Parabola EyelidSegmentator::segmentUpper(const Image* image, int pupilRadius)
+Parabola EyelidSegmentator::segmentUpper(const Image* image, int x0, int y0, int x1, int y1, const Circle& pupilCircle, const Circle& irisCircle)
 {
-	CvMat tmp;
-	cvGetSubRect(image, &tmp, cvRect(0, 0, image->width, image->height/2));
-	//TODO: improve this so we don't have to cvCreateImage() every time
-	Image* upperPart = cvCreateImage(cvGetSize(&tmp), IPL_DEPTH_32F, 1);
-	cvConvert(&tmp, upperPart);
-
 	Parabola bestParabola;
 	double maxGrad = INT_MIN;
 
 	for (int p = 150; p < 300; p += 50) {
-	//for (int p = 200; p < 201; p++) {
-		//std::pair<Parabola, double> res = this->findParabola(upperPart, p, 0, upperPart->height-pupilRadius);
-		std::pair<Parabola, double> res = this->findParabola(upperPart, p, 0, upperPart->height);
+		std::pair<Parabola, double> res = this->findParabola(image, p, x0, y0, x1, y1);
 		if (res.second > maxGrad) {
 			maxGrad = res.second;
 			bestParabola = res.first;
 		}
 	}
 
-	cvReleaseImage(&upperPart);
 	return bestParabola;
 }
 
-Parabola EyelidSegmentator::segmentLower(const Image* image, int pupilRadius)
+Parabola EyelidSegmentator::segmentLower(const Image* image, int x0, int y0, int x1, int y1, const Circle& pupilCircle, const Circle& irisCircle)
 {
-	CvMat tmp;
-	cvGetSubRect(image, &tmp, cvRect(0, image->height/2, image->width, image->height/2));
-	//TODO: improve this so we don't have to cvCreateImage() every time
-	Image* lowerPart = cvCreateImage(cvGetSize(&tmp), IPL_DEPTH_32F, 1);
-	cvConvert(&tmp, lowerPart);
-
 	Parabola bestParabola;
 	double maxGrad = INT_MIN;
 
 	for (int p = -300; p < -150; p += 50) {
-		//std::pair<Parabola, double> res = this->findParabola(lowerPart, p, pupilRadius, lowerPart->height);
-		std::pair<Parabola, double> res = this->findParabola(lowerPart, p, 0, lowerPart->height);
+		std::pair<Parabola, double> res = this->findParabola(image, p, x0, y0, x1, y1);
 		if (res.second > maxGrad) {
 			maxGrad = res.second;
 			bestParabola = res.first;
 		}
 	}
 
-	cvReleaseImage(&lowerPart);
-	return Parabola(bestParabola.x0, bestParabola.y0+image->height/2, bestParabola.p);
+	return bestParabola;
 }
 
-std::pair<Parabola, double> EyelidSegmentator::findParabola(const Image* image, int p, int yMin, int yMax)
+std::pair<Parabola, double> EyelidSegmentator::findParabola(const Image* image, int p, int x0, int y0, int x1, int y1)
 {
 	IplImage* gradient = cvCreateImage(cvGetSize(image), IPL_DEPTH_32F, 1);
-	int xMin = 0;
-	int xMax = image->width-1;
 
 	int step = Parameters::getParameters()->parabolicDetectorStep;
 
-	CvMat* M = cvCreateMat((yMax-yMin)/step, (xMax-xMin)/step, CV_32F);
+	CvMat* M = cvCreateMat((y1-y0)/step, (x1-x0)/step, CV_32F);
 
 	cvSobel(image, gradient, 0, 1, 7);
 	cvSmooth(gradient, gradient, CV_BLUR, 15);
 
 	for (int i = 0; i < M->rows; i++) {
-		int y0 = yMin+i*step;
+		int y = y0+i*step;
 		for (int j = 0; j < M->cols; j++) {
-			int x0 = xMin+j*step;
-			double avg = this->parabolaAverage(gradient, image, Parabola(x0, y0, p));
+			int x = x0+j*step;
+			double avg = this->parabolaAverage(gradient, image, Parabola(x, y, p));
 			cvSetReal2D(M, i, j, avg);
 		}
 	}
@@ -135,10 +114,10 @@ std::pair<Parabola, double> EyelidSegmentator::findParabola(const Image* image, 
 	cvReleaseMat(&Dy);
 	cvReleaseMat(&D);
 
-	int x0 = xMin+maxPos.x*step;
-	int y0 = yMin+maxPos.y*step;
+	int x = x0+maxPos.x*step;
+	int y = y0+maxPos.y*step;
 
-	return std::pair<Parabola, double>(Parabola(x0, y0, p), max);
+	return std::pair<Parabola, double>(Parabola(x, y, p), max);
 }
 
 double EyelidSegmentator::parabolaAverage(const Image* gradient, const Image* originalImage, const Parabola& parabola)
--- eyelidsegmentator.h
+++ eyelidsegmentator.h
@@ -19,10 +19,10 @@ public:
 	std::pair<Parabola, Parabola> segmentEyelids(const Image* image, const Circle& pupilCircle, const Circle& irisCircle);
 
 private:
-	Parabola segmentUpper(const Image* image, int pupilRadius);
-	Parabola segmentLower(const Image* image, int pupilRadius);
+	Parabola segmentUpper(const Image* image, int x0, int y0, int x1, int y1, const Circle& pupilCircle, const Circle& irisCircle);
+	Parabola segmentLower(const Image* image, int x0, int y0, int x1, int y1, const Circle& pupilCircle, const Circle& irisCircle);
 
-	std::pair<Parabola, double> findParabola(const Image* image, int p, int yMin, int yMax);
+	std::pair<Parabola, double> findParabola(const Image* image, int p, int x0, int y0, int x1, int y1);
 	double parabolaAverage(const Image* gradient, const Image* originalImage, const Parabola& parabola);
 
 	int pupilRadius;
