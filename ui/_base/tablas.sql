b79db400e48eaf0a361f20264773542af1b23a3f
Marcelo Mottalli <marcelo@usuario-desktop.(none)>
2010-01-28 17:44:14 -0300

    New tables

--- ui/_base/tablas.sql
+++ ui/_base/tablas.sql
@@ -6,31 +6,3 @@ CREATE TABLE base_iris (
         codigo_gabor TEXT
 );
 
-CREATE TABLE comparaciones (
-        id_imagen1 INTEGER NOT NULL,
-        id_imagen2 INTEGER NOT NULL,
-        distancia FLOAT NOT NULL,
-        intra_clase INTEGER NOT NULL,
-        PRIMARY KEY(id_imagen1, id_imagen2)
-);
-
-
-CREATE TABLE comparaciones_a_contrario (
-        id_imagen1 INTEGER NOT NULL,
-        id_imagen2 INTEGER NOT NULL,
-        distancia FLOAT NOT NULL,
-        parte INTEGER NOT NULL,
-        intra_clase INTEGER NOT NULL,
-        PRIMARY KEY(id_imagen1, id_imagen2, parte)
-);
-
-CREATE INDEX caa_idx ON comparaciones_a_contrario(id_imagen1);
-
-CREATE TABLE nfa_a_contrario (
-        id_imagen1 INTEGER NOT NULL,
-        id_imagen2 INTEGER NOT NULL,
-        nfa FLOAT NOT NULL,
-        intra_clase INTEGER NOT NULL,
-        PRIMARY KEY(id_imagen1, id_imagen2)
-);
-
