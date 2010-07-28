d7bda1e1b2fd6b03bcef824e777ddaa101fec113
Marcelo Mottalli <mottalli@yahoo.com>
2010-07-28 10:14:00 -0300

    Improved statistics

--- scripts/test_performance.py
+++ scripts/test_performance.py
@@ -6,6 +6,9 @@ import horus
 import Database
 from pylab import *
 
+def matchesPorSegundo(v):
+	return (1000.0*v[-1, 0])/v[-1, 1];
+
 base = Database.getDatabase('bath')
 #irisDatabase = horus.IrisDatabase()
 irisDatabaseCUDA = horus.IrisDatabaseCUDA()
@@ -13,12 +16,14 @@ irisDatabase = horus.IrisDatabase()
 
 step = 1000
 cant_total = 20000
+#cant_total = 5000
 
 codigo_base = base.conn.execute('SELECT codigo_gabor FROM base_iris WHERE segmentacion_correcta=1').fetchone()
 codigo_base = horus.unserializeIrisTemplate(str(codigo_base[0]))
 
 count = 0
 print "Tamaño | Tiempo match | Tiempo a contrario | Tiempo match CUDA | Tiempo a contrario CUDA | Dif. prom. HD | Dif. prom. NFA"
+res = []
 while count < cant_total:
 	rows = base.conn.execute('SELECT id_imagen,codigo_gabor FROM base_iris WHERE segmentacion_correcta=1')
 	for row in rows:
@@ -47,5 +52,23 @@ while count < cant_total:
 			
 			error = abs(mean(rd-rdcuda))
 			errorcuda = abs(mean(rnfa-rnfacuda))
+			
+			res.append([count, matchTime, aContrarioMatchTime, matchTimeCUDA, aContrarioMatchTimeCUDA, error, errorcuda])
 
 			print '%i, %.4f, %.4f, %.4f, %.4f %.8f, %.8f' % (count, matchTime, aContrarioMatchTime, matchTimeCUDA, aContrarioMatchTimeCUDA, error, errorcuda)
+
+res = array(res)
+
+print 'Matches por segundo (HD): ', matchesPorSegundo(res[:, [0,1]])
+print 'Matches por segundo (A Contrario): ', matchesPorSegundo(res[:, [0,2]])
+print 'Matches por segundo (HD c/CUDA): ', matchesPorSegundo(res[:, [0,3]])
+print 'Matches por segundo (A Contrario c/CUDA): ', matchesPorSegundo(res[:, [0,4]])
+
+plot(res[:, 0], res[:, 1], label='HD')
+plot(res[:, 0], res[:, 2], label='A Contrario')
+plot(res[:, 0], res[:, 3], label='HD w/CUDA')
+plot(res[:, 0], res[:, 4], label='A Contrario w/CUDA')
+xlabel('Database size')
+ylabel('Matching time (ms.)')
+legend()
+show()
