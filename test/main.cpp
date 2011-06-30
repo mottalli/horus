cf9d6d8bf4e479021bf1b5688e8d633b8cd48785
Marcelo Mottalli <mottalli@yahoo.com>
2011-06-30 11:44:20 -0300

    Removed qmake support (replaced by CMake support), cleaned up CMake support

--- test/main.cpp
+++ test/main.cpp
@@ -11,37 +11,45 @@ using namespace std;
 using namespace cv;
 using namespace horus;
 
+Segmentator segmentator;
+VideoProcessor videoProcessor;
+Decorator decorator;
+LogGaborEncoder encoder;
+
+void procesarImagen(Mat& imagen)
+{
+	Mat imagenBW;
+	cvtColor(imagen, imagenBW, CV_BGR2GRAY);
+	SegmentationResult sr = segmentator.segmentImage(imagenBW);
+	decorator.drawSegmentationResult(imagen, sr);
+
+	IrisTemplate irisTemplate = encoder.generateTemplate(imagenBW, sr);
+	decorator.drawTemplate(imagen, irisTemplate);
+
+	imshow("imagen", imagen);
+}
+
 int main(int, char**)
 {
-	VideoCapture cap(0);
+	/*VideoCapture cap(0);
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 720);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 576);
 
-	Segmentator segmentator;
-	VideoProcessor videoProcessor;
-	Decorator decorator;
-	LogGaborEncoder encoder;
-
 	Mat tmp, frame;
-	GrayscaleImage frameBW;
-	//Mat1b imagen = imread("/home/marcelo/iris/horus/base-iris/15_2.jpg", 0);
 
 	do {
 		cap >> tmp;
 		flip(tmp, tmp, 1);
 		frame = tmp(Rect(10, 0, tmp.cols-2*10, tmp.rows));
-		cvtColor(frame, frameBW, CV_BGR2GRAY);
 
-		SegmentationResult sr = segmentator.segmentImage(frameBW);
-		decorator.drawSegmentationResult(frame, sr);
-		//cout << segmentator.segmentationTime << endl;
+		procesarImagen(frame);
 
-		IrisTemplate irisTemplate = encoder.generateTemplate(frameBW, sr);
-		decorator.drawTemplate(frame, irisTemplate);
+;
+	} while (char(waitKey(5)) != 'q');*/
 
-		imshow("video", frame);
-	} while (char(waitKey(5)) != 'q');
-
+	Mat imagen = imread("/home/marcelo/iris/horus/base-iris/15_2.jpg", 1);
+	procesarImagen(imagen);
+	while (char(waitKey(5)) != 'q') {};
 
 	return 0;
 }
