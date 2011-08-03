52466e2420d0dfb277bb1d3a8144f6b859db700e
Marcelo Mottalli <mottalli@yahoo.com>
2011-08-03 11:21:16 -0300

    Android

---
+++ android/src/com/mm/horus/HorusActivity.java
@@ -0,0 +1,15 @@
+package com.mm.horus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HorusActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
